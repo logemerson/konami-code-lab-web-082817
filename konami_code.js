8acd16863c5ee1aff42298f40b0a035fc980f387
github username <logemerson@gmail.com>
2017-10-15 18:20:55 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let i = 0;
 
+  function contraCode(e) {
+    const stroke = parseInt(e.which || e.detail);
+
+    if (stroke === code[i]) {
+      i++;
+      if (i === code.length) {
+        alert("You've gained 30 lives! Shoot a gun a bunch.");
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  }
+  document.body.addEventListener("keydown", contraCode);
 }
